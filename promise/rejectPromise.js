383379ced9a7ac59d626544eebe2698a89273cc4
cshekharc <cshekhars.choudhari@gmail.com>
2024-02-01 21:40:10 +0530

    err

--- promise/rejectPromise.js
+++ promise/rejectPromise.js
@@ -1,7 +1,7 @@
 let prom = new Promise(function(resolve, reject) {
     setTimeout(function() {
         let err = false
-        if(err){
+        if(!err){
             resolve({userName: "Myself", password: "password"})
         }else{
             reject("error something went wrong")
@@ -9,8 +9,8 @@ let prom = new Promise(function(resolve, reject) {
     }, 1000)
 })
 
-let user = prom.then(function(user){
-    console.log(user)
+let user = prom.then(function(ar1){
+    console.log(ar1)
     // return user.userName
 }).catch(function(error){
     console.log(error)
